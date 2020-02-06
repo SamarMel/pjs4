d0db2e4fa22439957986163c5b4c90eefd8a68ef
andrealiss2000 <andrealiss@hotmail.fr>
2020-02-06 14:25:55 +0100

    maj
    css
    html

---
+++ Forum/accueil_forum.js
@@ -0,0 +1,9 @@
+
+var sujetInput = document.getElementById("sujetInput");
+
+
+function chgBorders(){
+    sujetInput.style.borderColor : "black";
+}
+
+sujetInput.addEventListener("click",chgBorders);

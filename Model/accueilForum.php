b750362731827814dca734508283876906c21607
andrealiss2000 <andrealiss@hotmail.fr>
2020-02-13 14:57:51 +0100

    Corrections d'erreur

--- Model/accueilForum.php
+++ Model/accueilForum.php
@@ -1,4 +1,4 @@
-<?php>
+<?php
 
 /**
  * Recherche d'un topic en fonction de ce qu'à demandé le user
@@ -19,11 +19,10 @@ function rechercherTopic($sujetRechercher){
         }
     }catch(PDOException $e){
         echo utf8_encode("Echec de recherche de topic : " $e->getMessage(). "\n");
-        //LANCER UNE ERREUR 404
         return false;
     }
 
-return true;
+      return true;
 
 
 }
@@ -39,14 +38,9 @@ return true;
          $query = $dpo->prepare($sql);
          $query->bindParam(':titre', $titreSujet);
          $query->bindParam(':idAuteur', $_SESSION['idAuteur']);
-         $query->bindParam(':dateTopic', SYSDATE();
+         $query->bindParam(':dateTopic', SYSDATE());
 
          $bool = $query->execute();
-         if($bool){
-            $resultat=$query->fetchAll(PDO::FETCH_ASSO);
-            $_SESSION["creationTopic"] = $resultat;
-           
-        }
          
 
      }catch(PDOException $e){
@@ -54,7 +48,7 @@ return true;
          return false;
      }
 
-     return $bool;
+     return true;
 
  }
 
@@ -63,7 +57,7 @@ return true;
   */
 
   function derniersTopic(){
-      require("./Model/connectSQL.php"); 
+      require("./connectSQL.php"); 
       try{
           $sql="SELECT * FROM Topic";
           $query= $pdo->prepare($sql);
@@ -71,7 +65,6 @@ return true;
           if($bool){
               $resultat=$query->fetchAll(PDO::FETCH_ASSO);
               $_SESSION["derniersTopics"] = $resultat;
-             
           }
         }catch(PDOException $e){
               echo utf8_encode("Echec de select : " .$e->getMessage() . "\n");
